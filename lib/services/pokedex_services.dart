50261fc7a8817a6b4c910ff9078e37e35a344a20
Alish Giri <alishgiri.92@gmail.com>
2020-06-22 16:40:24 +0545

    Add pokemon info api call to service file

--- lib/services/pokedex_services.dart
+++ lib/services/pokedex_services.dart
@@ -1,4 +1,6 @@
+import 'package:pokedex/models/pokemon.dart';
 import 'package:pokedex/models/pokemon_list.dart';
+import 'package:pokedex/utils/url_constants.dart';
 import 'package:pokedex/services/base_service.dart';
 
 class PokedexServices {
@@ -8,8 +10,13 @@ class PokedexServices {
   static final _instance = PokedexServices._internal();
 
   Future<PokemonList> getPokemons({int offset, int limit}) async {
-    final res = await dio
-        .get("https://pokeapi.co/api/v2/pokemon?offset=$offset&limit=$limit");
+    final res = await dio.get(apiGetPokemons(offset, limit));
     return PokemonList.fromJSON(res.data);
   }
+
+  Future<Pokemon> getPokemonInfo({int pokemonId}) async {
+    final res = await dio.get(apiPokemonInfo(pokemonId));
+    return Pokemon.fromJSON(res.data);
+  }
+
 }
